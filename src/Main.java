e4a5a24f5e9730b5f24d11650729d047cf2d1cb9
Abdulmalik Elghedafi <2420753@uwtsd@ac.uk>
2024-10-25 12:01:35 +0100

    Finished swapping numbers exercise

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+
+public class Main {
+    private static final Scanner stdin = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.print("Enter the first number: ");
+        int userInput1 = stdin.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int userInput2 = stdin.nextInt();
+
+        int[] userArray = new int[2];
+        userArray[0] = userInput1;
+        userArray[1] = userInput2;
+
+        int tempValue;
+        tempValue = userArray[0];
+        userArray[0] = userArray[1];
+        userArray[1] = tempValue;
+
+
+
+
+
+        for (int i = 0; i < userArray.length; i++){
+            if (i == 0) {
+                System.out.print("Swapping the numbers now.\n");
+                System.out.print("The first index is " + userArray[0] + "\n");
+
+            } else {
+                System.out.print("The second index is: " + userArray[1]);
+            }
+        }
+    }
+
+}
